b5e2db654e1bbe33bc1ebb8241ff6fbdcb2a50c7
louisjohnbichard <louisjohnbichard@gmail.com>
2014-09-29 17:28:40 +0100

    added timeline

--- js/freelancer.js
+++ js/freelancer.js
@@ -9,22 +9,15 @@ $(function() {
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top - 40
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
+        $(this).closest('.nav').find('.active').removeClass('active');
+        $(this).parent().addClass('active');
+
     });
 });
 
-// Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
-});
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
