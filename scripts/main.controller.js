d480ad522399db4b25c31db8b7fc094943e35c64
louisjohnbichard <louisjohnbichard@gmail.com>
2014-12-14 14:07:36 +0000

    added responsive homepage

--- scripts/main.controller.js
+++ scripts/main.controller.js
@@ -1,8 +1,9 @@
 portfolio.controller('mainController', function($scope, $location) {
-   
+
     // USED FOR THE ACTIVE TABS ON THE DASHBOARD
     $scope.$on('$locationChangeSuccess', function() {
         $scope.location = $location.path()
             .split('/')[1];
     });
+
 });
