d506f2e34b148ca2e9e44fd718c09d2a19f907a8
louisjohnbichard <louisjohnbichard@gmail.com>
2014-12-26 10:49:11 +0000

    added some blogs

--- scripts/blog.controller.js
+++ scripts/blog.controller.js
@@ -2,60 +2,62 @@ portfolio.controller('blogController', function($scope, $location, $route) {
 
     $scope.searchText = $route.current.params.blog_title;
 
-    $scope.posts = [
-        //"blog/brogrammer_targets.md",
-        //
-        {
-            id: "BL129",
-            author: 'Louis John Bichard',
-            date: '14th December 2014',
-            path: "blog/aggregate_animations.md",
-            title: 'Aggregating CSS animations for elegant transitions',
-            tags: ['CSS', 'Styles', 'Animations']
-        }, {
-            id: "BL128",
-            author: 'Louis John Bichard',
-            date: '14th December 2014',
-            path: "blog/blog_tags.md",
-            title: 'Angular: How I made the blog tags functionality for this site',
-            tags: ['Functional', 'Javascript', 'lodash', 'Underscore', 'Chaining', 'Angular']
-        }, {
-            id: "BL127",
-            author: 'Louis John Bichard',
-            date: '14th December 2014',
-            path: "blog/a_template_for_style_guides.md",
-            title: 'A template for style guides',
-            tags: ['CSS', 'SASS', 'Styles', 'Style guides', 'Material Design', 'Web Design', 'Design', 'Typography']
-        }, {
-            id: "BL126",
-            author: 'Louis John Bichard',
-            date: '14th December 2014',
-            path: "blog/the_power_of_nosql.md",
-            title: 'The Power Of NoSQL',
-            tags: ['Javascript', 'NoSQL', 'Databasing', 'New technologies', 'Technology', 'MongoDB']
-        }, {
-            id: "BL125",
-            author: 'Louis John Bichard',
-            date: '22th November 2014',
-            path: "blog/brogrammer_nutrient_timing.md",
-            title: 'The Brogrammer: Nutrient timing',
-            tags: ['Javascript', 'Brogrammer', 'Nutrients']
-        }, {
-            id: "BL124",
-            author: 'Louis John Bichard',
-            path: "blog/javascriptunittestexplained.md",
-            date: '4th December 2014',
-            title: 'Javascript Unit Tests Spies Explained',
-            tags: ['Javascript', 'Unit testing', 'Testing', 'Development', 'Programming', 'Efficiency']
-        }, {
-            id: "BL123",
-            author: 'Louis John Bichard',
-            date: '14th November 2014',
-            path: "blog/brogrammer_patience.md",
-            title: "The Brogrammer: Patience",
-            tags: ['Brogrammer', 'Patience']
-        }
-    ];
+    $scope.posts = [{
+        author: 'Louis John Bichard',
+        date: '23rd December 2014',
+        path: "blog/patternanalysis.md",
+        title: 'The Brogrammer series: Pattern analysis',
+        tags: ['Pattern Analysis', 'Brogrammer']
+    }, {
+        author: 'Louis John Bichard',
+        date: '23rd December 2014',
+        path: "blog/parkinsonslaw.md",
+        title: 'The Brogrammer series: Parkinsons Law',
+        tags: ['Brogrammer', 'Success', 'Parkinsons Law']
+    }, {
+        author: 'Louis John Bichard',
+        date: '14th December 2014',
+        path: "blog/aggregate_animations.md",
+        title: 'Aggregating CSS animations for elegant transitions',
+        tags: ['CSS', 'Styles', 'Animations']
+    }, {
+        author: 'Louis John Bichard',
+        date: '14th December 2014',
+        path: "blog/blog_tags.md",
+        title: 'Angular: How I made the blog tags functionality for this site',
+        tags: ['Functional', 'Javascript', 'lodash', 'Underscore', 'Chaining', 'Angular']
+    }, {
+        author: 'Louis John Bichard',
+        date: '14th December 2014',
+        path: "blog/a_template_for_style_guides.md",
+        title: 'A template for style guides',
+        tags: ['CSS', 'SASS', 'Styles', 'Style guides', 'Material Design', 'Web Design', 'Design', 'Typography']
+    }, {
+        author: 'Louis John Bichard',
+        date: '14th December 2014',
+        path: "blog/the_power_of_nosql.md",
+        title: 'The Power Of NoSQL',
+        tags: ['Javascript', 'NoSQL', 'Databasing', 'New technologies', 'Technology', 'MongoDB']
+    }, {
+        author: 'Louis John Bichard',
+        date: '22th November 2014',
+        path: "blog/brogrammer_nutrient_timing.md",
+        title: 'The Brogrammer series: Nutrient timing',
+        tags: ['Javascript', 'Brogrammer', 'Nutrients']
+    }, {
+        author: 'Louis John Bichard',
+        path: "blog/javascriptunittestexplained.md",
+        date: '4th December 2014',
+        title: 'Javascript Unit Tests Spies Explained',
+        tags: ['Javascript', 'Unit testing', 'Testing', 'Development', 'Programming', 'Efficiency']
+    }, {
+
+        author: 'Louis John Bichard',
+        date: '14th November 2014',
+        path: "blog/brogrammer_patience.md",
+        title: "The Brogrammer series: Patience",
+        tags: ['Brogrammer', 'Patience']
+    }];
 
     $scope.url = window.location.hostname + '/#blog/';
 
